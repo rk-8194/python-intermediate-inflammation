ae80fdd3221b784ed0eadbb86b92fba8788f4cb6
Ryan Kerr <ryan.kerr@ukaea.uk>
2024-10-08 14:09:51 +0100

    Added test cases for daily_min() and daily_max()

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,8 @@
 import os
 import numpy as np
 import numpy.testing as npt
-from inflammation.models import daily_mean, load_json
+import pytest
+from inflammation.models import daily_mean, load_json, daily_min, daily_max
 
 
 def test_daily_mean_zeros():
@@ -25,8 +26,47 @@ def test_daily_mean_integers():
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
 
+def test_daily_min_zero():
+    """Test script to get the minimum from the data set."""
+    test_input = np.array([[0, 0], [1, 1], [1, 1]])
+    test_result = np.array([0, 0])
+
+    npt.assert_array_equal(daily_min(test_input), test_result)
+
+
+def test_daily_min():
+    """Test script to get the minimum from the data set."""
+    test_input = np.array([[1, 2], [3, 4], [5, 0]])
+    test_result = np.array([1, 0])
+
+    npt.assert_array_equal(daily_min(test_input), test_result)
+
+
+def test_daily_max_zero():
+    """Test script to get the maximum from the data set."""
+    test_input = np.array([[1, 1], [0, 0], [0, 0]])
+    test_result = np.array([1, 1])
+
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    with pytest.raises(TypeError):
+        error_expected = daily_min([["Hello", "there"], ["General", "Kenobi"]])
+        print(error_expected)
+
+
+def test_daily_max():
+    """Test script to get the minimum from the data set."""
+    test_input = np.array([[1, 2], [3, 4], [5, 0]])
+    test_result = np.array([5, 4])
+
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
 def test_load_from_json(tmpdir):
-    """Test scrip to load from a json file.
+    """Test script to load from a json file.
 
     Args:
         tmpdir (string): Path to the json file.
